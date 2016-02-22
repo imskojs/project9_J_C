ab9f3a711b65faea689ec4ee1d927db160cdeb13
Seunghoon Ko <imskojs@gmail.com>
2016-02-22 10:10:48 +0900

    home lay

--- codes/js/app.js
+++ codes/js/app.js
@@ -30,7 +30,7 @@
     angular.extend($rootScope, RootScope);
 
     // setInitialState();
-    $state.go('Main.RequestCreate');
+    $state.go('Main.Footer.Home');
 
     $ionicPlatform.ready(onIonicPlatformReady);
 
