3a9a7d4f7d75d5955677837527442539006e0ab9
Seunghoon Ko <imskojs@gmail.com>
2016-02-22 13:00:16 +0900

    (feat)

--- codes/state/1Footer/FooterController.js
+++ codes/state/1Footer/FooterController.js
@@ -4,18 +4,62 @@
     .controller('FooterController', FooterController);
 
   FooterController.$inject = [
+    '$rootScope', '$state',
     'FooterModel'
   ];
 
   function FooterController(
+    $rootScope, $state,
     FooterModel
   ) {
     var Footer = this;
     Footer.Model = FooterModel;
 
-
+    Footer.goToState = goToState;
     //====================================================
     //  Implementation
     //====================================================
+    function goToState(state, params) {
+      // Home
+      if ($state.includes('Main.Footer.Home')) {
+        return $rootScope.goToState(state, params, 'forward');
+        // 
+      } else if ($state.includes('Main.Footer.EventTab.JoodangEventList')) {
+        if (state === 'Main.Footer.Home') {
+          return $rootScope.goToState(state, params, 'back');
+        } else {
+          return $rootScope.goToState(state, params, 'forward');
+        }
+        //Main.Footer.SearchTab.ProvinceList
+      } else if ($state.includes('Main.Footer.SearchTab.ProvinceList')) {
+        if (state === 'Main.Footer.Home' ||
+          state === 'Main.Footer.EventTab.JoodangEventList'
+        ) {
+          return $rootScope.goToState(state, params, 'back');
+        } else {
+          return $rootScope.goToState(state, params, 'forward');
+        }
+      } else if ($state.includes('Main.Footer.TalkList')) {
+        if (state === 'Main.Footer.Home' ||
+          state === 'Main.Footer.EventTab.JoodangEventList' ||
+          state === 'Main.Footer.SearchTab.ProvinceList'
+        ) {
+          return $rootScope.goToState(state, params, 'back');
+        } else {
+          return $rootScope.goToState(state, params, 'forward');
+        }
+      } else if ($state.includes('Main.Footer.SettingList')) {
+        if (state === 'Main.Footer.Home' ||
+          state === 'Main.Footer.EventTab.JoodangEventList' ||
+          state === 'Main.Footer.SearchTap.ProvinceList' ||
+          state === 'Main.Footer.TalkList'
+        ) {
+          return $rootScope.goToState(state, params, 'back');
+        } else {
+          return $rootScope.goToState(state, params, 'forward');
+        }
+
+      }
+    }
   }
 })();
