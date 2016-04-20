34798cd9abe42794e27d47f09cf135fc0db234df
Rex <rex@Rexui-MacBook-Pro.local>
2016-04-20 16:52:41 +0900

    -bash joodang 1.1.0 android

--- codes/state/CompanyCreate/CompanyCreateController.js
+++ codes/state/CompanyCreate/CompanyCreateController.js
@@ -6,14 +6,14 @@
   CompanyCreateController.$inject = [
     '_MockData',
     '$ionicHistory', '$scope', '$state', '$window',
-    'CompanyCreateModel', 'RootScope', 'Users', 'Util'
+    'CompanyCreateModel', 'RootScope', 'Users', 'Util', 'Message'
   ]; //Controller함수에 factory로 생성된 model을 주입(factory 이름).
   //동일한 app 모듈에 선언한 factory이기 때문에 주입받을 수 있다.
 
   function CompanyCreateController(
     _MockData,
     $ionicHistory, $scope, $state, $window,
-    CompanyCreateModel, RootScope, Users, Util
+    CompanyCreateModel, RootScope, Users, Util, Message
   ) {
     var initPromise;
     var noLoadingStates = [];
--- codes/state/RequestCreate/RequestCreateController.js
+++ codes/state/RequestCreate/RequestCreateController.js
@@ -6,14 +6,14 @@
   RequestCreateController.$inject = [
     '_MockData',
     '$ionicHistory', '$scope', '$state', '$window',
-    'RequestCreateModel', 'RootScope', 'Users', 'Util'
+    'RequestCreateModel', 'RootScope', 'Users', 'Util', 'Message'
   ]; //Controller함수에 factory로 생성된 model을 주입(factory 이름).
   //동일한 app 모듈에 선언한 factory이기 때문에 주입받을 수 있다.
 
   function RequestCreateController(
     _MockData,
     $ionicHistory, $scope, $state, $window,
-    RequestCreateModel, RootScope, Users, Util
+    RequestCreateModel, RootScope, Users, Util, Message
   ) {
     var initPromise;
     var noLoadingStates = [];
